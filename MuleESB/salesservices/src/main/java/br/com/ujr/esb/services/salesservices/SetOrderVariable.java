a0d9019e5005c86c7bbd3fe6f2163629cebf95d2
Ualter Junior <ualter.junior@gmail.com>
2015-02-01 19:06:14 -0200

    .

---
+++ MuleESB/salesservices/src/main/java/br/com/ujr/esb/services/salesservices/SetOrderVariable.java
@@ -0,0 +1,23 @@
+package br.com.ujr.esb.services.salesservices;
+
+import org.codehaus.jackson.map.DeserializationConfig.Feature;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.mule.api.MuleEventContext;
+import org.mule.api.lifecycle.Callable;
+
+import br.com.ujr.isus.canonical.Order;
+
+public class SetOrderVariable implements Callable {
+
+	@Override
+	public Object onCall(MuleEventContext eventContext) throws Exception {
+		String restOrder = eventContext.getMessage().getPayloadAsString().replaceAll("order", "Order"); 
+		ObjectMapper om = new ObjectMapper();
+		om.configure(Feature.UNWRAP_ROOT_VALUE, true);
+		Order order = om.readValue(restOrder, Order.class);
+		System.out.println(order);
+		
+		return eventContext.getMessage().getPayload();
+	}
+
+}
